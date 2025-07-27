799695b60e55fb4f38729e695d5476df7e846850
ratneshranve <ratneshranve@gmail.com>
2025-07-27 22:02:19 +0530

    hii

--- src/Components/NavComponent/Nav.js
+++ src/Components/NavComponent/Nav.js
@@ -11,15 +11,21 @@ function Nav() {
           <li><Link to="/resume">Resume</Link></li>
         </ul>
       </div>
+
       <div id="logo">
-        <img id="image" src="assets/images/logo1.png" alt="logo" />
+        <img
+          id="image"
+          src={`${process.env.PUBLIC_URL}/assets/images/logo1.png`}
+          alt="logo"
+        />
       </div>
+
       <div id="link">
         <ul className="flex space-x-4">
           <li>
             <Link to="/skills">
               <img
-                src="assets/images/skills.png"
+                src={`${process.env.PUBLIC_URL}/assets/images/skills.png`}
                 alt="Skills"
                 style={{ maxWidth: '50px', maxHeight: '50px', width: 'auto', height: 'auto' }}
               />
@@ -27,22 +33,29 @@ function Nav() {
           </li>
           <li>
             <Link to="/projects">
-              <img src="assets/images/project.png" alt="Projects" className="w-8 h-8" />
+              <img
+                src={`${process.env.PUBLIC_URL}/assets/images/project.png`}
+                alt="Projects"
+                className="w-8 h-8"
+              />
             </Link>
           </li>
           <li>
             <Link to="/contact">
-              <img src="assets/images/contact.png" alt="Contact" className="w-8 h-8" />
+              <img
+                src={`${process.env.PUBLIC_URL}/assets/images/contact.png`}
+                alt="Contact"
+                className="w-8 h-8"
+              />
             </Link>
           </li>
-          
         </ul>
       </div>
 
-      {/* Add your profile photo here */}
+      {/* Profile Photo */}
       <div className="nav-profile-photo">
         <img
-          src="/assets/images/ratnesh.jpg"   // <-- change filename as appropriate
+          src={`${process.env.PUBLIC_URL}/assets/images/ratnesh.jpg`}
           alt="Ratnesh Ranve"
           className="profile-photo"
         />
--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+ <BrowserRouter basename="/portfolio">
+  
     <App />
   </BrowserRouter>
 );
