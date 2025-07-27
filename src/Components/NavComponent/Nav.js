73e0acf7dffaefa2899f92934054e9bde27bccaf
ratneshranve <ratneshranve@gmail.com>
2025-07-27 21:47:40 +0530

    images

--- src/Components/NavComponent/Nav.js
+++ src/Components/NavComponent/Nav.js
@@ -12,14 +12,14 @@ function Nav() {
         </ul>
       </div>
       <div id="logo">
-        <img id="image" src="/assets/images/logo1.png" alt="logo" />
+        <img id="image" src="assets/images/logo1.png" alt="logo" />
       </div>
       <div id="link">
         <ul className="flex space-x-4">
           <li>
             <Link to="/skills">
               <img
-                src="/assets/images/skills.png"
+                src="assets/images/skills.png"
                 alt="Skills"
                 style={{ maxWidth: '50px', maxHeight: '50px', width: 'auto', height: 'auto' }}
               />
@@ -27,12 +27,12 @@ function Nav() {
           </li>
           <li>
             <Link to="/projects">
-              <img src="/assets/images/project.png" alt="Projects" className="w-8 h-8" />
+              <img src="assets/images/project.png" alt="Projects" className="w-8 h-8" />
             </Link>
           </li>
           <li>
             <Link to="/contact">
-              <img src="/assets/images/contact.png" alt="Contact" className="w-8 h-8" />
+              <img src="assets/images/contact.png" alt="Contact" className="w-8 h-8" />
             </Link>
           </li>
           
